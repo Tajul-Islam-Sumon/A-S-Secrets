1aa7f200bdda0671905bab92e0873d603a31c97f
Tajul Islam Sumon <tajul.islam@northsouth.edu>
2024-01-22 21:51:26 +0600

    Adding Users New Secrects Page

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String, 
-    googleId: String
+    googleId: String,
+    secret: String
 }); 
 
 userSchema.plugin(passportLocalMongoose); 
@@ -89,12 +90,46 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
+    User.find({"secret": {$ne: null}})
+    .then((foundUsers) =>{
+        if(foundUsers){
+            res.render("secrets", {usersWithSecrets: foundUsers});
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+app.get("/submit", function(req, res){
     if(req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     }
     else{
         res.redirect("/login");
     }
+});
+
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret; 
+
+    User.findById(req.user.id)
+    .then((foundUser) => {
+        if(foundUser){
+            foundUser.secret = submittedSecret;
+
+            foundUser.save()
+            .then(()=>{
+                res.redirect("/secrets");
+            })
+            .catch((err)=>{
+                console.log(err);
+            });
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
 
 app.get("/logout", function(req, res){
